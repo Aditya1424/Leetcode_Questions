a1923a2be09b49e50389ce190397934e8be75c6f
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2023-02-12 03:18:52 +0530

    Create Fibonacci_Number.java

---
+++ Fibonacci_Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0|| n==1){
+            return n;
+        }
+        int a = fib(n-1);
+        int b=fib(n-2);
+        return a+b;
+    }
+}
