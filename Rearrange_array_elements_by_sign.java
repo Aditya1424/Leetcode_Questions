27e2cf02e54d84fde800f16609f32aa2528ca6d0
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2023-02-17 20:01:10 +0530

    Create Rearrange_array_elements_by_sign.java

---
+++ Rearrange_array_elements_by_sign.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int arr[] = new int[nums.length];
+        int even =0;
+        int odd=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                arr[even]=nums[i];
+                even+=2;
+            }
+            else{
+                arr[odd]=nums[i];
+                odd+=2;
+            }
+        }
+        return arr;
+    }
+}
