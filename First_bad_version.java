26c3675d97fcbace493259ecfb34a951d23e8a9f
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2023-02-21 23:17:08 +0530

    Create First_bad_version.java

---
+++ First_bad_version.java
@@ -0,0 +1,19 @@
+ public class Solution extends VersionControl{
+    public int firstBadVersion(int n) {
+        int low =1;
+        int high=n;
+        int ans=0;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isBadVersion(mid)==true){
+                ans = mid;
+                high = mid-1;
+                
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
