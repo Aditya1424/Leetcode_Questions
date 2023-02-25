17edc8763c9f7108f961c8877fc6fc7bde9d5093
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2023-02-25 16:55:36 +0530

    Create Valid_perfect_square.java

---
+++ Valid_perfect_square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low = 1;
+        long high =num;
+        while(low<=high){
+            long mid = low+(high-low)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid>num){
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+}
