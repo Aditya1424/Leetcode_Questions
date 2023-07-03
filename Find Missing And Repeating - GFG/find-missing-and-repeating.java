ab35cf1764f12d4928121b59187928b6bf88be8b
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2023-07-04 00:49:32 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        int ans[]= new int[2];
+        
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                ans[0]=arr[i];
+            }
+        }
+        int sum = 1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == sum)
+                sum++;
+        }
+        
+            
+        
+        ans[1]=sum;
+        return ans;
+    }
+}
