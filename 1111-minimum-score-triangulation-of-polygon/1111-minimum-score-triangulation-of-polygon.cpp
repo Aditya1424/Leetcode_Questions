e35b22e584e2dade2585b78905bda67c1f61fe26
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2024-08-21 16:04:50 +0530

    Time: 3 ms (74.22%), Space: 10.3 MB (49.37%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -20,10 +20,32 @@ public:
 
     }
 
-    int minScoreTriangulation(vector<int>& values) {
-        int n= values.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+    int solveTab(vector<int>& v ){
+        int n= v.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+2;j<n;j++){
+
+                int ans = INT_MAX;
+                for(int k=i+1;k<j;k++){
+                    ans = min(ans, v[i]*v[k]*v[j]+dp[i][k]+dp[k][j]);
+                }
+                dp[i][j]= ans;
+            }
+        }
+
+        return dp[0][n-1];
         
-        return solve(values,0,n-1,dp);
+
+    }
+
+    int minScoreTriangulation(vector<int>& values) {
+        // int n= values.size();
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        
+        // return solve(values,0,n-1,dp);
+
+        return solveTab(values);
     }
 };
