f0965e81b783e056ed4ddefde7c85b514bb11c38
ADITYA GUPTA <92393365+Aditya1424@users.noreply.github.com>
2025-06-21 02:21:28 +0530

    Time: 0 ms (100%), Space: 44.4 MB (90.09%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+
+        if(head==null || head.next==null){
+            return head;
+        }
+        
+        ListNode odd= head;
+        ListNode even= head.next;
+        ListNode evenHead= even;
+
+        // even pointer will always be ahead of odd pointer
+        // so the condition will be on even pointer only
+        while(even!=null && even.next!=null){
+            odd.next= odd.next.next;
+            even.next= even.next.next;
+
+            // move the pointer ahead
+            odd= odd.next;
+            even= even.next;
+        }
+
+        odd.next= evenHead;
+        return head;
+    }
+}
